de43f5e1ab31804410cf364a1ab0336274d51ec2
richardmaccaw <ricci6@gmail.com>
2018-11-05 16:08:46 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,14 @@
 ActiveRecord::Schema.define(version: 20181105155341) do
 
   create_table "passengers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "passenger_id"
-    t.integer  "taxi_id"
+    t.integer "passenger_id"
+    t.integer "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
